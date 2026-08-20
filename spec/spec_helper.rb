a91bab2913f5dc4bc2139ae18157c2d013549c90
HassanAkbar <hassanakbar2@gmail.com>
2026-08-20 19:06:22 +0500

    wire golden tasks, spec loading, and CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -61,6 +61,8 @@ end
 
 require "elkrb"
 
+Dir[File.join(__dir__, "support/**/*.rb")].reject { |f| f.end_with?("_spec.rb") }.each { |f| require f }
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
